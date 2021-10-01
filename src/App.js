675665a2897c1eb609ea1386fe9917ddceab5e38
sonickaka <sonickaka@gmail.com>
2021-10-01 20:41:35 +0900

    fixed

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
+       /*
+		  <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
@@ -17,6 +18,8 @@ function App() {
         >
 		niceshot!
         </a>
+		
+		*/
       </header>
     </div>
   );
